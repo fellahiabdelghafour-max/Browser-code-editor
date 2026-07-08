07f1dcaf7d5e2e2f80678236421d781f779a9ce9
abdelghafour <fellahiabdelghafour@gmail.com>
2026-07-08 07:56:39 -0700

    second commit

--- hook/ThemeMode/useThemeMode.ts
+++ hook/ThemeMode/useThemeMode.ts
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
 export default function useThemeMode(){
-    
-const LocalMode=localStorage.getItem('mode') as 'light'|'dark'|null;
-    const [mode,setMode]=useState<'light'|'dark'>(LocalMode || 'dark');
+const [mode, setMode] = useState<'light' | 'dark'>('dark');
+
+useEffect(() => {
+  const localMode = localStorage.getItem("mode") as "light" | "dark" | null;
+
+  if (localMode) {
+    setMode(localMode);
+  }
+}, []);
     useEffect(()=>{
         localStorage.setItem('mode',mode);
     },[mode]);
